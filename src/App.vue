cda67aa5c6c2c25119983c5e1f7a35a3d24bfe44
Steven de Valk <steven.devalk@atos.net>
2023-08-11 16:12:11 +0200

    Added reset.min.scss, importet reset.min.scss in App.vue

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import url("./assets/styles/reset.min.scss");
+
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
