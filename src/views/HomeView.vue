b6c9d3dd1385ad1134d3756f8b25bf50c82f9ef5
Steven de Valk <steven.devalk@atos.net>
2023-08-14 10:38:26 +0200

    Added styling and placeholder welcome text to HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,53 @@
 <template>
     <div class="home">
-        <h1>Welcome!</h1>
+        <h1>Welcome to Foosball!</h1>
+        <p>
+            Hello and welcome to Foosball!, the app that lets you enjoy the fun
+            and excitement of table soccer on your mobile device! 🎮
+        </p>
+        <p>
+            With Foosball!, you can register players, create games and see the
+            individual scores of each player and view a leaderboard. Whether you
+            want to play solo, challenge your friends, or join a tournament,
+            Foosball! has something for everyone. 🙌
+        </p>
+        <p>
+            To get started, you need to create an account and choose a username
+            and password. Then you can register players by entering their names
+            and selecting their avatars. You can also customize your players'
+            skills, such as speed, accuracy, and power. 🚀
+        </p>
+        <p>
+            Once you have registered your players, you can create games by
+            choosing the number of players, the game mode, and the difficulty
+            level. You can also invite other players to join your game or join a
+            game created by someone else. You can play online or offline,
+            depending on your preference. 🌐
+        </p>
+        <p>
+            As you play games, you will earn points and badges based on your
+            performance. You can see the individual scores of each player and
+            view a leaderboard that shows the rankings of all the players in
+            Foosball!. You can also compare your stats with other players and see
+            how you stack up against them. 💯
+        </p>
+        <p>
+            Foosball! is more than just an app, it's a community of table soccer
+            enthusiasts who share a passion for the game. You can chat with
+            other players, send them messages, and give them feedback. You can
+            also follow your favorite players and see their latest updates. 💬
+        </p>
     </div>
 </template>
+
+<style lang="scss" scoped>
+.home {
+    margin: 2rem;
+    padding: 2rem;
+    background-color: #ceedf8;
+
+    p {
+        margin: 1rem 0;
+    }
+}
+</style>
